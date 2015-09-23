f0ac5236458c86eabe1b8573dc4c9b2f7e01a588
Pei Pei <Pei.Pei@D2L.com>
2015-09-23 11:45:01 -0400

    LongPress and tap will result messages printed to concole

--- LongPressGesture/LongPressGesture/ViewController.swift
+++ LongPressGesture/LongPressGesture/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    //UIImage does not reponde to long gesture recongnizer
+    
+    @IBAction func handleLongPre(sender: AnyObject) {
+        if sender.state == UIGestureRecognizerState.Began
+        {
+            println("LongPress_begin")
+        } else if (sender.state == UIGestureRecognizerState.Ended){
+            println("LongPress_Ended")
+        }
+    }
+    
+    @IBAction func handTap(sender: AnyObject) {
+        
+            println("tap_Ended")
+          }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
